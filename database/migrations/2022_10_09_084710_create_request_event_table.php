e00e4400b2f21bbc3323bf22131b9bbc64fb2d6a
adibn22s <adib.shodiq@gmail.com>
2022-10-09 16:04:28 +0700

    fixed table

--- database/migrations/2022_10_09_084710_create_request_event_table.php
+++ database/migrations/2022_10_09_084710_create_request_event_table.php
@@ -17,12 +17,14 @@ return new class extends Migration
             $table->id();
             $table->enum('role',[1,2,3,4,5]);
             $table->string('instance');
-            $table->string('event_name');
+            $table->string('event_name')-> unique();
             $table->enum('category',[1,2,3]);
             $table->string('invite_group_link');
-            $table->date('time_is_held');
+            $table->date('date_is_held');
             $table->string('description');
             $table->longText('poster')->nullable();
+            $table->date('added_date')->nullable();
+            $table->time('added_time')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
